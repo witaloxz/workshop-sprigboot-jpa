99e2fc58481f023f736858ccf4349bda581ce68c
witaloxz <witalodias1@gmail.com>
2025-06-17 16:27:30 -0300

    User entity and resource

---
+++ src/main/java/com/witalo/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.witalo.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.witalo.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "witalo", "witalo@gmail.com", "666666666", "666");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+
+}
