5dab7578f263e39864bc2f3dac9581260a0657f3
witaloxz <witalodias1@gmail.com>
2025-06-19 17:09:02 -0300

    OrderStatus enum

--- src/main/java/com/witalo/course/config/TestConfig.java
+++ src/main/java/com/witalo/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.witalo.course.entities.Order;
 import com.witalo.course.entities.User;
+import com.witalo.course.entities.enums.OrderStatus;
 import com.witalo.course.repositories.OrderRepository;
 import com.witalo.course.repositories.UserRepository;
 
@@ -29,8 +30,8 @@ public class TestConfig implements CommandLineRunner {
 		User us2 = new User(null, "Andressa", "andressa@gmail.com", "40028922", "123456");
 		
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), us1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), us2);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID ,us1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT ,us2);
 		
 		userRepository.saveAll(Arrays.asList(us1,us2));
 		orderRepository.saveAll(Arrays.asList(o1,o2));
